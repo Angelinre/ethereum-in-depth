580910afb02ece26c85e34d4ca68ee57042277ef
Angelinre <57348916+Angelinre@users.noreply.github.com>
2019-11-04 11:31:42 +0100

    Update MyContract.sol

--- 1-contract-creation/contracts/MyContract.sol
+++ 1-contract-creation/contracts/MyContract.sol
@@ -11,3 +11,8 @@ contract MyContract {
     return a + b;
   }
 }
+truffle(develop)> compile
+truffle(develop)> sender = web3.eht.accounts
+[0]
+truffle(develop)> opts = { from: sender, to: null, data: MyContract.bytecode, gas: 4600000}
+truffle(develop)> txHash = web3.eth.sendTransaction(opts)
