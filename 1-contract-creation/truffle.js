d86416b7f25de710488d090f760986945230c044
Angelinre <57348916+Angelinre@users.noreply.github.com>
2019-11-04 11:43:07 +0100

    Update truffle.js

--- 1-contract-creation/truffle.js
+++ 1-contract-creation/truffle.js
@@ -1,1 +1,6 @@
 module.exports = {}
+truffle(develop)> compile
+truffle(develop)> sender = web3.eth.accounts
+[0]
+truffle(develop)> opts = { from: sender, to: null, data: MyContract.bytecode, gas: 4600000 }
+truffle(develop)> txHash = web.eth.sendTransaction(opts)
